a5fdba146e6f1aa8469570f91687ab4989bd1c9a
Thatamsetty Thrinethra <thrinethra098@gmail.com>
2026-01-29 11:06:00 +0530

    Final clean deploy-ready backend

--- processing_app/models.py
+++
@@ -1,8 +0,0 @@
-from mongoengine import Document, StringField, BooleanField, DictField
-
-class ProjectStatus(Document):
-    project_id = StringField(required=True)
-    active = BooleanField(default=True)
-    running = BooleanField(default=False)
-    completed = BooleanField(default=False)
-    result = DictField()
